1ee414bc55e500b6fe834930794b6d672f439986
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-05-12 11:35:36 +0300

    test

--- app/serverLocation.js
+++ app/serverLocation.js
@@ -1,1 +1,2 @@
-export const serverLocation = location.origin.includes(':8080') ? 'https://localhost:3000' : location.origin;
+export const serverLocation = location.origin;
+  // .includes(':8080') ? 'http://localhost:3000' : location.origin;
